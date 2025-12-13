abb93023f13165d977d1e2f97d2c5ee127c2d460
Christian WONG <wongchristian68@gmail.com>
2025-12-13 17:04:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3018279448-24395",
+  "appId": "1:714451940794:web:71fc07158c26cd19711983",
+  "apiKey": "AIzaSyAJcTVazTvqEDJDTi7zqQAwfAonX3NDcrY",
+  "authDomain": "studio-3018279448-24395.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "714451940794"
+};
